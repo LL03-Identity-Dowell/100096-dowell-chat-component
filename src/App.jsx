7ea862ad94220cd8f08961e34235a45c38a80e79
libanabduba <habib.gemechu@aait.edu.et>
2024-02-06 00:38:41 +0300

    update in order of file/import name changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Home from "./components/Home/Home";
+import DowellChat from "./components/Home/DowellChat";
 
 function App() {
   return (
     <>
-      <Home position={"fixed bottom-5 left-6 ml-3 z-50"} />
+      <DowellChat
+        position={"fixed bottom-5 left-6 ml-3 z-50"}
+      />
     </>
   );
 }
