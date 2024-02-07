83641c125150314934d2edf7b0b606db726cc217
libanabduba <habib.gemechu@aait.edu.et>
2024-02-07 03:08:19 +0300

    Implemented: component and styles for individual messages

---
+++ src/components/Chat/components/ChatMessage.jsx
@@ -0,0 +1,20 @@
+import styles from './ChatMessageStyle'; // Style the component in a separate CSS file
+
+const ChatMessage = ({ sender, message, time }) => {
+  return (
+    <>
+    <div  style={sender === 'user' ? styles.userMessage  : styles.botMessage }>
+    {sender != 'user' && <div style={{...styles.botMessageBefore, ...styles.botMessageBeforeLeft}}></div>}
+      <div style={sender === 'user' ? styles.headerUser : styles.headerBot }>
+        <div style={styles.messageSender}>{sender}</div>
+        <div style={styles.messageTime}>{time}</div>
+      </div>
+      <div style={styles.messageText}>{message}</div>
+      
+    {sender === 'user' && <div style={{...styles.userMessageBeforeAfter, ...styles.userMessageAfter}}></div>}
+    </div>
+    </>
+  );
+};
+
+export default ChatMessage;
